a0346ec3a569200d3733f2cd575d4215a6052839
fernandoaraujo5184 <a5184@alunos.ipca.pt>
2015-04-01 20:48:31 +0100

    Update mailbox.rb

---
+++ exercise2/mailbox.rb
@@ -0,0 +1,37 @@
+class Email
+  def initialize(subject,headers)
+    @subject = subject
+    @date = headers[:date]
+    @from = headers[:from]
+  end
+  
+  def subject
+    @subject
+  end
+  
+  def date
+    @date
+  end
+  
+  def from
+    @from
+  end
+end
+
+class Mailbox
+  # fill in this class body
+end
+
+emails = [
+  Email.new("Homework this week", { :date => "2014-12-01", :from => "Ferdous" }),
+  Email.new("Keep on coding! :)", { :date => "2014-12-01", :from => "Dajana" }),
+  Email.new("Re: Homework this week", { :date => "2014-12-02", :from => "Ariane" })
+]
+mailbox = Mailbox.new("Ruby Study Group", emails)
+
+mailbox.emails.each do |email|
+  puts "Date:    #{email.date}"
+  puts "From:    #{email.from}"
+  puts "Subject: #{email.subject}"
+  puts
+end
